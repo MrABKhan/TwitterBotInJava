78bf603a0617f5b1df553b90c960d6e63aa650a2
abu.bakar <theawesomekhan@gmail.com>
2018-12-20 14:48:00 +0300

    Added Skeleton base for Tweeting App.

---
+++ TwitterBot/src/TwitterBot.java
@@ -0,0 +1,32 @@
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
+import java.io.InputStream;
+
+public class TwitterBot {
+
+    public static void main(){
+
+    }
+
+    private static void TweetFromSource(){
+        InputStream inputStream = null; // To Be Added.
+
+        //Insert Method for getting tweets after sentiment analysis
+    }
+
+    private static void SendTweet(String inputTweet){
+        Twitter twitter = TwitterFactory.getSingleton();
+        Status status;
+
+        try {
+            status = twitter.updateStatus(inputTweet);
+            System.out.println("Sending Status: " + status);
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
