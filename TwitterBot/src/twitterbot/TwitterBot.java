66c9e811e40b3657e93039d7038f938fdd634d55
abu.bakar <theawesomekhan@gmail.com>
2018-12-20 15:09:01 +0300

    Updated Exceptions and Added FileStream Readers for the tweets.

--- TwitterBot/src/twitterbot/TwitterBot.java
+++ TwitterBot/src/twitterbot/TwitterBot.java
@@ -5,16 +5,28 @@ import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
-import java.io.InputStream;
+import java.io.*;
 
 public class TwitterBot {
 
     public static void main(){
-
+        TweetFromSource();
     }
 
     private static void TweetFromSource(){
-        InputStream inputStream = null; // To Be Added.
+        try {
+            InputStream inputStream = new FileInputStream("C:\\gitrepo\\TwitterBotInJava\\TwitterBot\\resources\\tweets.txt"); // To Be Added.
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+
+
+        } catch (FileNotFoundException e) {
+
+            System.out.println("File Not Found Exception");
+
+            e.printStackTrace();
+
+        }
 
         //Insert Method for getting tweets after sentiment analysis
     }
@@ -27,6 +39,7 @@ public class TwitterBot {
             status = twitter.updateStatus(inputTweet);
             System.out.println("Sending Status: " + status);
         } catch (TwitterException e) {
+            System.out.println("Exception related to twitter4j");
             e.printStackTrace();
         }
     }
